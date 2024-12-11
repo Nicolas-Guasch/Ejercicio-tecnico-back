7c0ca6568691fc96d53e26dfc41b359177f7d781
Nicolas-Guasch <nic.guasch@gmail.com>
2024-12-11 18:16:34 -0300

    Remove Client.Id set

--- Client.cs
+++ Client.cs
@@ -4,7 +4,7 @@
 */
 public class Client
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Address { get; set; }
